32543dad7d7e20da4a9b9854e9b4210e20dbfe36
MA82F <fahimi13823182@gmail.com>
2025-09-11 09:39:47 +0330

    chat added

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Message
+from .serializers import MessageSerializer
 
-# Create your views here.
+class MessageListCreateView(generics.ListCreateAPIView):
+	serializer_class = MessageSerializer
+	permission_classes = [permissions.IsAuthenticated]
+
+	def get_queryset(self):
+		return Message.objects.filter(user=self.request.user).order_by('-created_at')
+
+	def perform_create(self, serializer):
+		serializer.save(user=self.request.user)
+
+class MessageDetailView(generics.RetrieveAPIView):
+	queryset = Message.objects.all()
+	serializer_class = MessageSerializer
+	permission_classes = [permissions.IsAuthenticated]
