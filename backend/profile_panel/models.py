dc464c921039dccdf9486355f432c0e72cc60e95
Mehdi-Es84 <mhtiasta@gmail.com>
2025-09-13 12:44:27 +0330

    final test back

--- backend/profile_panel/models.py
+++ backend/profile_panel/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.core.validators import FileExtensionValidator
 from login_signup.models import User
+from django.core.exceptions import ValidationError
 
 class UploadedFile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    file = models.FileField(upload_to='uploads/', validators=[FileExtensionValidator(allowed_extensions=['pdf', 'xlsx', 'xls' , 'jpg', 'jpeg', 'png'])])
-
+    file = models.FileField(upload_to='uploads/', validators=[FileExtensionValidator(allowed_extensions=['.xlsx', '.xls' ,'.csv'])])
     uploaded_at = models.DateTimeField(auto_now_add=True)
-
+    
     def __str__(self):
         return f"{self.user.username} - {self.file.name}"
