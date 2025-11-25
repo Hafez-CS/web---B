2bef4574beea0fbfee5874162974e5f484a499e8
MohammadAmin Fahimi <fahimi13823182@gmail.com>
2025-11-25 09:35:19 +0330

    nothing important

--- backend/economic/urls.py
+++ backend/economic/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import get_dollar, get_bors, get_gold, get_bitcoin, get_all_currencies
 
 urlpatterns = [
-    path('dollar/', get_dollar, name='get_dollar'),
-    path('gold/', get_gold, name='get_gold'),
-    path('bors/', get_bors, name='get_bors'),
-    path('bitcoin/', get_bitcoin, name='get_bitcoin'),
-    path('all/', get_all_currencies, name='get_all_currencies'),
+    path('dollar', get_dollar, name='get_dollar'),
+    path('gold', get_gold, name='get_gold'),
+    path('bors', get_bors, name='get_bors'),
+    path('bitcoin', get_bitcoin, name='get_bitcoin'),
+    path('all', get_all_currencies, name='get_all_currencies'),
 ]
